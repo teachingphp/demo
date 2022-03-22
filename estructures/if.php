64a5700215428dc5f97b601a7f278717a4aba4a8
alberto <peacocksteam@gmail.com>
2022-03-22 14:13:03 +0100

    Noves estructures control

---
+++ estructures/if.php
@@ -0,0 +1,15 @@
+/* Codi exemple if */
+<?php
+
+    $a = 5;
+    $b = 10;
+
+    if ($a > $b) {
+        echo "a es mayor que b";
+    } elseif ($a == $b) {
+        echo "a es igual que b";
+    } else {
+        echo "a es menor que b";
+    }
+
+?>
---
+++ estructures/switch.php
@@ -0,0 +1,19 @@
+
+/* Codi exemple switch */
+<?php
+
+    $i = 1;
+
+    switch ($i) {
+        case 0:
+            echo "i es igual a 0";
+            break;
+        case 1:
+            echo "i es igual a 1";
+            break;
+        default:
+            echo "i no es 0 o 1";
+            break;
+    }
+
+?>
