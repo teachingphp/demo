37467647998ab41d4a75c2c5a757a9970c3b5c75
marcellaceina <php.ceina.marcella@gmail.com>
2022-03-24 13:51:06 +0100

    ghgh

--- estructures/foreach.php
+++ estructures/foreach.php
@@ -1,11 +1,7 @@
 /* Codi exemple foreach per mostrar tots els elements d'un array */
 <?php
 
-<<<<<<< HEAD
     $a = array(1, 2, 3, 17, 45);
-=======
-    $a = array(1, 2, 3, 17, 45, 50);
->>>>>>> a75f544b99a9617998e4c9028b603b06efae4379
 
     foreach ($a as $valor) {
         echo $valor;
