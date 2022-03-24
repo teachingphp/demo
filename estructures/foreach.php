a75f544b99a9617998e4c9028b603b06efae4379
CEINApol <php.ceina.pol@gmail.com>
2022-03-24 13:37:44 +0100

    test

--- estructures/foreach.php
+++ estructures/foreach.php
@@ -1,7 +1,7 @@
 /* Codi exemple foreach per mostrar tots els elements d'un array */
 <?php
 
-    $a = array(1, 2, 3, 17, 45);
+    $a = array(1, 2, 3, 17, 45, 50);
 
     foreach ($a as $valor) {
         echo $valor;
