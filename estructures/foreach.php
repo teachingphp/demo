fdae8bdd46effa1de2214045dc8d562bda72304d
SybelleZ <php.ceina.albertc@gmail.com>
2022-03-24 13:43:41 +0100

    Commit

--- estructures/foreach.php
+++ estructures/foreach.php
@@ -1,7 +1,7 @@
 /* Codi exemple foreach per mostrar tots els elements d'un array */
 <?php
 
-    $a = array(1, 2, 3, 17);
+    $a = array(1, 2, 3, 67);
 
     foreach ($a as $valor) {
         echo $valor;
