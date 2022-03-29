835d14d3cc60f0bc2c7e2d86f44ee892a5c4dce2
AlbertViSa <php.ceina.albertvilla@gmail.com>
2022-03-29 13:54:46 +0200

    nueva_lista

--- webpages/ListaFichero.php
+++ webpages/ListaFichero.php
@@ -3,29 +3,22 @@
     <title>Leer Fichero</title>
 </head>
 <body>
-<h1 style="text-align:center;" >Llista dels alumnes de la classe</h1>
+<h1 style="text-align:center;background-color:DodgerBlue;color:#3F51B5">Llista dels alumnes de la classe</h1>
 <?php
     $miFichero = fopen("../recursos/listaAlumnos.txt", "r") or die("No se puede abrir el fichero!");
-    $noies = array("Andrea","Ana","Marcela");
+    $noies = array("Andrea","Ana","Marcela","Albert_V");
 
     while(!feof($miFichero)){
         $nombreAlumno = str_replace("\r\n","",fgets($miFichero));
         if (in_array($nombreAlumno,$noies)){
-            echo "<h2 >" . $nombreAlumno . "</h2>";
-            echo '<img  src="../recursos/programmerw.png" onclick ="alert(2)" alt="" width="100" height="100">';
+            echo '<h2 style="text-align:center" >' . $nombreAlumno . '</h2>';
+            echo '<img  src="../recursos/programmer.png" onclick ="alert(2)" alt="" width="100" height="100">';
          }
         else{
-            echo "<h2 >" . $nombreAlumno . "</h2>";
+            echo '<h2 style="text-align:center" >' . $nombreAlumno . '</h2>';
             echo '<img  src="../recursos/programmer.png" onclick ="alert(2)" alt="" width="100" height="100">';
-        }
     }
-
-
     fclose($miFichero);
-    
 ?>
-    
-
-
 </body>
 </html>
