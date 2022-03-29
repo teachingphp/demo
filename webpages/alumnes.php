e58e5c7ca4c7d0ebd225fb0ff7bf6d6d3e4dad98
marcellaceina <php.ceina.marcella@gmail.com>
2022-03-29 11:57:14 +0200

    Merge branch 'main' of https://github.com/teachingphp/demo

--- webpages/alumnes.php
+++ webpages/alumnes.php
@@ -6,11 +6,11 @@
 <h1 style="text-align:center;" >LLista dels alumnes de la classe</h1>
 <?php
 
-    $nom_alumnes = array("Albert C", "Andrea", "Oscar", "Anas","FALTO JO :ALBERT XD");
+    $nom_alumnes = array("Albert C", "Andrea", "Oscar", "Anas","FALTO JO :ALBERT XD", "Marcel·la");
 
     foreach ($nom_alumnes as $nom){
         echo "<h2 >" . $nom . "</h2>";
-        echo '<img  src="../recursos/programmer.png" onclick ="alert(2)" alt="" width="100" height="100">';
+        echo '<img  src="../recursos/programmerw.png" onclick ="alert(2)" alt="" width="100" height="100">';
     }
 
 ?>
