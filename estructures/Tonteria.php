43b7bca8aed67d4141890324ecd97cee751ac207
SybelleZ <php.ceina.albertc@gmail.com>
2022-03-29 10:05:54 +0200

    Merge branch 'main' of https://github.com/teachingphp/demo

---
+++ estructures/Tonteria.php
@@ -0,0 +1,11 @@
+<?php
+    echo "<img src='./will-smith-chris-rock-28032022-2.gif' >";
+?>
+<html>
+    <head>
+        <title>Slapped mah boi</title>
+        <video width="700" height="450" controls>
+  <source src="Bonk.mp4" type="video/mp4">
+
+
+</video>
