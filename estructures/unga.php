069845fc6ab66c08253337edf0d4474fd5d66bc7
dani3456 <php.ceina.daniel@gmail.com>
2022-03-29 12:34:35 +0200

    imagien el mordo

--- estructures/unga.php
+++ estructures/unga.php
@@ -5,7 +5,9 @@
         <title>El fin se acerca</title>
         <h2 style="color:blueviolet">voy a por ustedes cabrones</h2>
         <video width="500" height="300" controls>
-           
+        <h1>The img element</h1>
+
+<img src="https://revistamutaciones.com/wp-content/uploads/2017/07/S6E24.028_Mordecai_and_Rigby_Driving_in_a_Car.png" alt="pajaro y mapache" width="500" height="600">
             <source src="Se van a morir cabrones vs Te voy a mat4r cabron MEME.mp4" type="video/mp4">
         </video> 
         <audio controls>
