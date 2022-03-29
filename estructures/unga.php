12d62ea5e9946b32743583e7458f005a58938dd6
dani3456 <php.ceina.daniel@gmail.com>
2022-03-29 11:48:22 +0200

    commit

--- estructures/unga.php
+++ estructures/unga.php
@@ -4,5 +4,6 @@
     <head>
         <title>El fin se acerca</title>
         <video width="500" height="300" controls>
+            <h2 style="color:blueviolet">voy a por ustedes</h2>
             <source src="Se van a morir cabrones vs Te voy a mat4r cabron MEME.mp4" type="video/mp4">
         </video> 
