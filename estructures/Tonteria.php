6d677f809c542ad3cb433491933275a235528570
SybelleZ <php.ceina.albertc@gmail.com>
2022-03-29 10:44:34 +0200

    commit

--- estructures/Tonteria.php
+++ estructures/Tonteria.php
@@ -5,7 +5,7 @@
     <head>
         <title>Slapped mah boi</title>
         <video width="700" height="450" controls>
-  <source src="../estructures/Bonk.mp4" type="video/mp4">
+  <source src="./Bonk.mp4" type="video/mp4">
 
 
 </video>
