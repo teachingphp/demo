81edf15d54c16e664edfee33af9eda0b9e0625f5
MarcelComes <php.ceina.marcel@gmail.com>
2022-03-31 12:02:34 +0200

    completed 2 php

--- webpages/marcel.php
+++ webpages/marcel.php
@@ -3,7 +3,17 @@
     <title>звезда</title>
 </head>
 <body>
-<u><h1 style="text-align:center;" ><B><FONT COLOR="red">Тесла звезда</h1></u></FONT>
-<p style="background-image: url('https://wallpaperswire2.nyc3.cdn.digitaloceanspaces.com/uploads/2021/07/Lsd-wallpapers-19.jpg');">
-<?php
+<i><h1 style="text-align:center;" ><B><FONT COLOR="red"><h3><p style="font-family:Cursive">Тесла звезда</p></h3></h1></i></FONT>
+<body>
+<body style="background-color:#000000;">
+</body>
 
+</head>
+<img src= "https://wallpaperswire2.nyc3.cdn.digitaloceanspaces.com/uploads/2021/07/Lsd-wallpapers-19.jpg" alt="Betelgeuse" width="1300" height="580">
+
+<html>
+   <head>
+      <title>Redirection</title>
+      <meta http-equiv = "refresh" content = "7; url = https://www.youtube.com/watch?v=fVtkAGJvNu8" />
+   </head>
+</html>
