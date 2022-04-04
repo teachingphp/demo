608db5f2a5a732236c0492c4c348e45c6d2d41b4
anaceina <php.ceina.ana@gmail.com>
2022-04-04 10:13:17 +0200

    cookies.php

---
+++ webpages/cookies.php
@@ -0,0 +1,15 @@
+<html>  
+<body>
+
+<h1>Introdueix les teves dades:</h1>
+<form action="resultat_login.php" method="post">
+    Nom artístic: <input type="text" name="name"><br><br>
+    Passa temps preferit: <input type="text" name="Passa temps preferit"><br><br>
+    Password: <input type="password" name="password"><br><br>
+    <input type="submit" name="submit" value="Envia">
+</form>
+<h2>Segon formulari</h2>
+
+
+</body>
+</html>
