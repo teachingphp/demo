feb688775b2e5ea6f4924189d5b201e66ba47365
dani3456 <php.ceina.daniel@gmail.com>
2022-04-04 13:50:47 +0200

    unga update

---
+++ webpages/recuperarUnga.php
@@ -0,0 +1,17 @@
+
+<?php
+?>
+<html>
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>50 troyanos quieren entrar en su servidor </h1>
+
+<form action="" method="post">
+  <label for="usuari">nombre:</label>
+  <input type="text" id="usuari" name="usuari"><br><br>
+  <label for="password">contrasenya:</label>
+  <input type="text" id="pasword" name="password"><br><br>
+  <input type="submit" value="enviar">
+</form>
