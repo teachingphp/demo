04cc243ae189d5c22aab5f60f3fec9f014b41517
FranOrellana <php.ceina.fran@gmail.com>
2022-04-04 10:18:03 +0200

    vgdcvb

--- estructures/Fran2.php
+++ estructures/Fran2.php
@@ -1,20 +1,15 @@
 <?php
-<html>
 
-<form> method="post" action="" name="signin-form">
-    <div class="form-element">
-        <label>Username</label>
-        <input type="text" name="username" pattern="[a-zA-Z0-9]+" required />
-    </div>
-    <div class="form-element">
-        <label>Password</label>
-        <input type="password" name="password" required />
-    </div>
-    <button type="submit" name="login" value="login">Log In</button>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ejemplo simple de login con PHP | parzibyte.me</title>
+</head>
 
-    </form>
 
-    </html>
+
 
 ?>
 
