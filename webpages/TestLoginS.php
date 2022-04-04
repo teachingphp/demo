173c5e6c0039d61bef536272d371855a0a9e44d4
SybelleZ <php.ceina.albertc@gmail.com>
2022-04-04 12:40:30 +0200

    Tot noice

--- webpages/TestLoginS.php
+++ webpages/TestLoginS.php
@@ -1,18 +1,14 @@
-<?php
-
-
-?>
-
+<!DOCTYPE html>
 <html>
     <head>
         <body>
-        <title>Registrat o Inicia Sessió</title>
-        <h1 style="color:mediumturquoise">Registrat o Inicia Sessió</h1>
+        <title>Registrat</title>
+        <h1 style="color:mediumturquoise">Registrat</h1>
         
         <p>Requereix aquestes dades:</p>
     <form action="../estructures/Tonteria.php" method="post">
         Usuari <input type="text" name="name"><br><br>
-        Email <input type="text" name="email"><br><br>
         Contrasenya <input type="password" name="password"><br><br>
+        
         <input type="submit" name="submit" value="Envia">
     </form>
