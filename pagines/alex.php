99875444b3678d999a538ffe224f08324b410001
alberto <peacocksteam@gmail.com>
2022-04-07 00:08:21 +0200

    Crear estructures pàgines personals

---
+++ pagines/alex.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>  
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="../css/alex.css?v=<?php echo time(); ?>">
+        <title>Alex</title>
+    </head>
+    <body>
+        <!--https://www.w3schools.com/tags/ -->  
+        <h1>La meva pàgina personal</h1>
+
+    </body>
+</html>
---
+++ pagines/marcella.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>  
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="../css/marcella.css?v=<?php echo time(); ?>">
+        <title>Marcel·la</title>
+    </head>
+    <body>
+        <!--https://www.w3schools.com/tags/ -->  
+        <h1>La meva pàgina personal</h1>
+
+    </body>
+</html>
