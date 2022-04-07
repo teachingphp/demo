156d3fc975f6e612d4ed933cc853ca52bf87a418
SybelleZ <php.ceina.albertc@gmail.com>
2022-04-07 13:12:16 +0200

    Cositas Pàgina Personal

--- pagines/albertc.php
+++ pagines/albertc.php
@@ -2,11 +2,19 @@
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="../css/albertc.css?v=<?php echo time(); ?>">
-        <title>AlbertC</title>
+        <title>DibusAlbertLandia</title>
     </head>
     <body>
         <!--https://www.w3schools.com/tags/ -->  
-        <h1>La meva pàgina personal</h1>
-
+        <h1>Benvinguts a DibusAlbertLandia</h1>
+        <audio id = "audio4" controls autoplay>
+  <source src="../recursos/Ghost Master Ghoul Room.mp3" type="audio/mpeg">
+</audio>
+<script type="text/javascript">
+    document.getElementById("audio4").style.visibility="hidden"
+</script>
     </body>
 </html>
+
+<div class="content-inner style=height:auto !important;">
+</div>
