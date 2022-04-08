22da4f8f2fcb28693322cc5af9b74c775bbca8fd
AlexPhPCeina <php.ceina.awlex@gmail.com>
2022-04-08 16:45:41 +0200

    afegit un article amb css poder personalitzar-ho

--- pagines/alex.php
+++ pagines/alex.php
@@ -5,7 +5,7 @@
         <title>Alex</title>
         <nav><ul>
         <li><a href="#">Inicio</a></li>
-        <li><a href="#">Quiénes somos</a></li>
+        <li><a href="http://localhost/demo/webpages/Cosesquiensomos.php">Quiénes somos</a></li>
         <li><a href="#">Servicios</a></li>
         <li><a href="#">Contactanos</a></li>
         <li><a href="http://localhost/demo/webpages/Coseslogin.php">Registrarse</a></li>
@@ -17,15 +17,26 @@
     <body>    
     <!--https://www.w3schools.com/tags/ --> 
         
-        <img src="https://www.w3schools.com/css/img_tree.png"style="float:left">  
+        <img class = "buo" src="https://www.w3schools.com/css/img_tree.png"style="float:left">  
         <h1>Benvingut A</h1>
         <h2>La meva pàgina personal</h2>
         <p>Aquesta es la meva primera paguina web estic treballant per avançar i poder millorar </p><br><br>
-        <hr>
-        <br><br><h1 style="text-align:Left;">Per millorar</h1><br><br>
+        <br><br><h1 style="text-align:left;">Per millorar</h1><br><br>
         <p class = "pT">Em Faltaria algun tipus de organitzacio de programacio per tindre-ho molt mes visual i amb millora eficient</p><br>
+        <hr>
+        <article class= "article">
+        <h1 class= "arh1">¿Quién inventó realmente la bombilla?</h1>        
+        <img src="https://www.endesa.com/content/dam/endesa-com/endesaclientes/blog/imagenes/1400033877351.jpg.resize.414.250.center.center.jpeg" width="669" height="350 " alt="Bombillas" style="vertical-align:center"><br><br>
+        <details>
+        <summary >Despliega aqui para descubrirlo</summary>
+        <p class= "article">1809: <b>Humphry Davy</b> coloca una fina tira de carbón entre los dos polos de una pila. Se considera el origen de la bombilla.</p><br>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Davy_lamp.png/250px-Davy_lamp.png" width="250" height="350 " alt="Lampara davy" style="vertical-align:center">
+        <p class="article">1840: <b>Warren de la Rue</b> coloca un filamento de platino en el interior de un tubo de vacío. Logra aumentar la duración de la luz, pero sus costes eran excesivos.</p><br>
 
 
+        </details>
+        </article>
+        
     </body>
 <!--<img src="https://yt3.ggpht.com/ytc/AKedOLS9O2Y3ywNf_YJ1PeDYk85FVXYit47yvDAfrTgC=s176-c-k-c0x00ffffff-no-rj-mo" alt="logo" width="50" height="50"  style="float:left">-->
 </html>
---
+++ webpages/Cosesquiensomos.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+    <title>Quiénes somos</title>
+    </head>
+    <body>
+     
+    <h1>Hola en esta seccion: </h1>
+    <h2>Nos mostraremos tal cual somos</h2><br>
+    
+    <img src="https://upload.wikimedia.org/wikipedia/commons/8/84/Ateles_fusciceps_Colombia.JPG"width="200" height="250">
+    <a> este es paco el desarollador de letras </a>   
+
+
+
+    </body>
+
+</html>
