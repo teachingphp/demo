4f4f724a5bf9860d7f52fbe4838a837cbd99668a
AlexPhPCeina <php.ceina.awlex@gmail.com>
2022-04-21 10:28:02 +0200

    Creacion de contactanos intentar poner el logo d'instagram

---
+++ webpages/Contactanos.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="../css/Contactanos.css?v=<?php echo time(); ?>">
+    <title>Contactanos</title>
+
+    </head>
+    <body>
+    <img src="instagram.png">
+
+</body>
+</html>
+
+     
