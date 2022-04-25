2ce1527ac744cf96d6209621dc887688b594b2dc
mrkuus22 <php.Ceina.Marc@gmail.com>
2022-04-25 09:34:04 +0200

    gt gt4gt4

--- pagines/marc.php
+++ pagines/marc.php
@@ -8,6 +8,7 @@
         <!--https://www.w3schools.com/tags/ -->  
         <h1>La meva pàgina personal</h1>
         <h2>Esto si es mi pagina</h2>
-
+        <p class = "pollo"> <ins><b>mama</b></ins></p>
+        
     </body>
 </html>
