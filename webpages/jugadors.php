8f2fff3e4c4934c7e1c3c0d949387355239806fb
AlexPhPCeina <php.ceina.awlex@gmail.com>
2022-04-26 13:39:55 +0200

    fotos de jugadores

--- webpages/jugadors.php
+++ webpages/jugadors.php
@@ -81,6 +81,7 @@ if ($mysqli -> connect_errno) {
     <path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3.854 2.146a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 3.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 7.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"/>
   </symbol>
 </svg>
+
  
 <div class="list-group">
   <?php
@@ -98,8 +99,7 @@ if ($mysqli -> connect_errno) {
         <p class="mb-0 opacity-75">descripcio de les preguntes respostes o respostes encertades</p>
       </div>
       <small class="opacity-50 text-nowrap">'.$value["IDALUM"].'</small>
-    </div>
-  </a>
+   
       </div>';
         echo $alumne;
       }
