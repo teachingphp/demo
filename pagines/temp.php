dc7cc469146f392b841b457d7cf7619a9a7117db
AlexPhPCeina <php.ceina.awlex@gmail.com>
2022-04-28 11:34:29 +0200

    cambi de estructura de temp

--- pagines/temp.php
+++ pagines/temp.php
@@ -1,29 +1,34 @@
 <html>
-<head>
-<fieldset>
-<legend>Time:</legend>
-<?php
- $time = 0;
-while($time ==  0)
- {
-    echo "El Tiempo es el ";
-    echo " Sieguente";
-    echo " ";
-    echo $time;
-    $time++;
- }   
-if($time ==  01)
-{
-    echo "el timepo es 10";
-    $time++;
-}
-elseif ($time == 02) 
-{
-    echo "El tiempo es 2";
-    $time ++;
-}
+   <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
 
-?>
-</fieldset>
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style> 
+<head>
+<link rel="stylesheet" href="../css/menuestils.css">
+  </head>
+  <body class="d-flex h-100 text-center text-white bg-dark">
+    
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="mb-auto">
+    <div>
+      <h3 class="float-md-start mb-0">Cover</h3>
+      <nav class="nav nav-masthead justify-content-center float-md-end">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Contact</a>
+      </nav>
+    </div>
+  </header>
 </head>
 </html>
