d27b786e4f3b0aadeeb82324469801dda809f763
mrkuus22 <php.Ceina.Marc@gmail.com>
2022-05-02 09:54:30 +0200

    jhbyuhujmni

--- pagines/marc.php
+++ pagines/marc.php
@@ -13,32 +13,40 @@
         <br>
         <div class="gallery">
   <a target="_Bloodborn" href="https://pivigames.blog/drak-souls-i-en-espanol-online-steam/">
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbz5BHC32llUvVN5aNduR-Vlq3V9qqe2q17Q&usqp=CAU" alt="Mono" width="225" height="225">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbz5BHC32llUvVN5aNduR-Vlq3V9qqe2q17Q&usqp=CAU" alt="Mono" width="600" height="400">
   </a>
-  <div class="desc">Este es <b >Paco</b> el desarollador de letras</div>
+  <div class="desc"> <b >Bloodborne</b></div>
 </div>
 
 <div class="gallery">
   <a target="_MAnue" href="https://thelastofus.es/">
     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSnDL7ucayIYmGACLZ9fPaEBpx8Mffg6cSrg&usqp=CAU" alt="Forest" width="600" height="400">
   </a>
-  <div class="desc">Este es <b>ManueH</b> el diseñador de Graficos</div>
+  <div class="desc"> <b>The last of us</b></div>
 </div>
 
 <div class="gallery">
   <a target="Phili i Milfi" href="https://www.rockstargames.com/es/gta-v">
     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdQGFUPM8BdwXTKygU22YYnniauUVf9XXxAw&usqp=CAU" alt="Northern Lights" width="600" height="400">
   </a>
-  <div class="desc"><b >Phili i Milfi</b> los de caricaturas</div>
+  <div class="desc"><b>Gta V</b> </div>
 </div>
 
 <div class="gallery">
-  <a target="PANCHO" href="img_mountains.jpg">
-    <img src="https://www.nationalgeographic.com.es/medio/2018/02/27/monos__1280x720.jpg" alt="monos" width="1280" height="720">
+  <a target="PANCHO" href="https://www.ubisoft.com/es-es/game/the-crew/the-crew-2">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNhiycEdJtBKioDmzrjehIoK0taBhhLT7QHw&usqp=CAU" alt="monos" width="1280" height="720">
   </a>
-  <div class="desc"><ins><b >Pancho  el modelo</b></ins></div>
+  <div class="desc"><ins><b >The crew 2</b></ins></div>
 </div>
 
+    <div class="gallery">
+    <a target="Phili i Milfi" href="https://dl2.dyinglightgame.com/es/">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNK8CO_WABEw3gzi3syV99TVW8w1WCsiWM6Q&usqp=CAU" alt="Northern Lights" width="600" height="400">
+     </a>
+     <div class="desc"><b>Dying light</b> 
+    </div>
     
+  
+
     </body>
 </html>
