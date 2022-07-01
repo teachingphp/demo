9be0d4822e81ada5f66d9b677d904558edc6f9ce
AlexPhPCeina <php.ceina.awlex@gmail.com>
2022-07-01 06:38:43 +0200

    Regalo

---
+++ Regalo/Regalo.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+    <title>Regalo para ti!!!</title>
+    <link rel="stylesheet" type="text/css" href="./Regalo.css?v=<?php echo time(); ?>">
+</head>
+<body>
+<br>
+  <h1>Bienvenido a la Pagina</h1>
+  <p>Leedlo todo plis</p>
+  
+<br>
+
+  <hr class ="hr">
+<button href="#">Este boton esta esperando una funcionalidad</button>
+
+<article>
+<p>Este boton sirve para reunir un grupo</p>
+
+<h2 style=" text-align: center;">un grupo que creara santimientos i alegrias,aparte de avances</h2>
+
+<h1>Porque el Señor que tenemos en el centro de todos nosotros nos ha dado todo este conocimiento, para poder realizar cosas pequeñas</h1>
+<p> Que esas cosas pequeñas las podemos conbertir en cosas <h1>Muy Grandes</h1> </p>
+
+<h2 style="text-align: center; color:white;">Acompañadme en esta aventura para poder agradecer todo el esfuerzo que el hico por nostros al enseñarnos</h2>
+
+
+<h2 style="text-align: center; color:blue;">Lo que hare sera dejar los archivos en el repositorio demo</h2>
+
+</article>
+
+
+
+
+
+
+
+</body>
+</html>
