a1964efa320576f551d4b3f9089ef95e3ce37b22
AlexPhPCeina <php.ceina.awlex@gmail.com>
2022-07-02 11:06:38 +0200

    corregido

--- Regalo/Regalo.php
+++ Regalo/Regalo.php
@@ -5,26 +5,27 @@
 </head>
 <body>
 <br>
-  <h1>Bienvenido a la Pagina</h1>
-  <p>Leedlo todo plis</p>
+  <h1>Bienvenido a la Página</h1>
+  <p>Leedlo todo please</p>
   
 <br>
 
   <hr class ="hr">
-<button href="#">Este boton esta esperando una funcionalidad</button>
+<button href="#">Este botón está esperando una funcionalidad</button>
 
 <article>
-<p>Este boton sirve para reunir un grupo</p>
+<p>Este botón sirve para reunir un grupo</p>
 
-<h2 style=" text-align: center;">un grupo que creara santimientos i alegrias,aparte de avances</h2>
+<h2 style=" text-align: center;">Un grupo que creara sentimientos y alegrías, aparte de avances
+</h2>
 
 <h1>Porque el Señor que tenemos en el centro de todos nosotros nos ha dado todo este conocimiento, para poder realizar cosas pequeñas</h1>
-<p> Que esas cosas pequeñas las podemos conbertir en cosas <h1>Muy Grandes</h1> </p>
+<p> Que esas cosas pequeñas las podemos convertir en cosas<h1>Muy Grandes</h1></p>
 
-<h2 style="text-align: center; color:white;">Acompañadme en esta aventura para poder agradecer todo el esfuerzo que el hico por nostros al enseñarnos</h2>
+<h2 style="text-align: center; color:white;">Acompañadme en esta aventura para poder agradecer todo el esfuerzo que él hizo por nosotros al enseñarnos</h2>
 
 
-<h2 style="text-align: center; color:blue;">Lo que hare sera dejar los archivos en el repositorio demo</h2>
+<h2 style="text-align: center; color:blue;">Lo que haré será dejar los archivos en el repositorio demo</h2>
 
 </article>
 
